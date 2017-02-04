def4e4c9ee1056ff79c76a817f9bf4abb94c29a4
sebi7224 <sebi7224@users.noreply.github.com>
2017-02-04 18:36:55 +0100

    Methode getPoints() hinzugefügt

--- core/src/de/emg_haar/schafkopfdeluxe/game/Card.java
+++ core/src/de/emg_haar/schafkopfdeluxe/game/Card.java
@@ -4,13 +4,11 @@ package de.emg_haar.schafkopfdeluxe.game;
  * Created by NoahB on 26.01.17.
  */
 
-public class Card
-{
+public class Card {
     private CardColor color;
     private CardRank rank;
 
-    public Card(CardColor c, CardRank r)
-    {
+    public Card(CardColor c, CardRank r) {
         color = c;
         rank = r;
     }
@@ -25,9 +23,7 @@ public class Card
         return color;
     }
 
-    public int getPoints()
-    {
-        return 0;
-        //Muss nachdem in CardRank die Points hinzugefügt wurden noch geändert werden
+    public int getPoints() {
+        return rank.getPoints();
     }
 }

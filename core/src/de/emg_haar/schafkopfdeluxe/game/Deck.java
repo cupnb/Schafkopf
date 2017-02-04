9d4eaed41b7f630b94b4caa1e1dac2f5528192bc
LukasDrse <lukas.droese@freenet.de>
2017-02-04 13:30:55 +0100

    unperfekte Mischmethode eingefuegt

--- core/src/de/emg_haar/schafkopfdeluxe/game/Deck.java
+++ core/src/de/emg_haar/schafkopfdeluxe/game/Deck.java
@@ -1,12 +1,15 @@
 package de.emg_haar.schafkopfdeluxe.game;
 
-import java.util.Collections;
+import java.util.Random;
 import java.util.Stack;
-
+import java.util.List;
+import java.util.Collections;
 
 public class Deck {
 
     private Stack<Card> cards = new Stack<>();
+    private List<Card> shuffleList = new List<>();
+    private List<Card> shuffleList2 = new List<>();
 
     public Deck() {
         //Karten werden erzeugt
@@ -23,7 +26,25 @@ public class Deck {
 
     public void initialize(Stack<Card> prevDumpedCards)
     {
-        //Hier kommt die Logik zum nicht perfekten Mischen rein
+        Random x = new Random();
+        int kartenRaus = x.nextInt(15) + 5;
+        Random y = new Random();
+        int kartenzahlStart = y.nextInt(31) - kartenRaus;
+        for (int i = 0;i < kartenzahlStart; i++) {
+            shuffleList.add(i, prevDumpedCards.pop());
+        }
+        for (int j = 0; j < kartenRaus; j++) {
+            shuffleList2.add(j,prevDumpedCards.pop());
+        }
+        for (int k = 0; k < 32-kartenRaus; k++) {
+            shuffleList.add(kartenzahlStart + k, prevDumpedCards.pop());
+        }
+        for (int l = 0; l < kartenRaus; l++) {
+            shuffleList.add(32 - kartenRaus, shuffleList2.remove(1));
+        }
+        for (int m = 0; m < 32; m++) {
+            prevDumpedCards.push(shuffleList.remove(1));
+        }
     }
 
     //Packt 4 Karten vom Stack in ein neues Stack und gibt dieses zurueck

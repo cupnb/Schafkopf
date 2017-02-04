30813aeff880441b5315e825bfb49a5488d3579b
alul_000 <alul@gmx.net>
2017-02-04 13:25:42 +0100

    Card initialisiert

--- core/src/de/emg_haar/schafkopfdeluxe/game/Card.java
+++ core/src/de/emg_haar/schafkopfdeluxe/game/Card.java
@@ -4,7 +4,30 @@ package de.emg_haar.schafkopfdeluxe.game;
  * Created by NoahB on 26.01.17.
  */
 
-public class Card {
+public class Card
+{
+    private CardColor color;
+    private CardRank rank;
 
+    public Card(CardColor c, CardRank r)
+    {
+        color = c;
+        rank = r;
+    }
 
+    public CardRank getRank()
+    {
+        return rank;
+    }
+
+    public CardColor getColor()
+    {
+        return color;
+    }
+
+    public int getPoints()
+    {
+        return 0;
+        //Muss nachdem in CardRank die Points hinzugefügt wurden noch geändert werden
+    }
 }

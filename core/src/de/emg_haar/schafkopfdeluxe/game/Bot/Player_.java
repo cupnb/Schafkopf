6155cce429e4e90b5cdee2246f6d1e4dfa9d7954
Alex <alexander.duca@gmail.com>
2017-04-16 12:55:01 +0200

    Abstrakte Klasse "Player_ "

---
+++ core/src/de/emg_haar/schafkopfdeluxe/game/Bot/Player_.java
@@ -0,0 +1,21 @@
+package de.emg_haar.schafkopfdeluxe.game.Bot;
+
+import de.emg_haar.schafkopfdeluxe.game.Game;
+import de.emg_haar.schafkopfdeluxe.game.card.Card;
+import java.util.List;
+
+public abstract class Player_ {
+
+    public abstract String getName();
+    public abstract void setGame(Game g);
+    public abstract void setPlayer();
+    public abstract boolean getPlayer();
+    public abstract boolean getContra();
+    public abstract boolean getRe();
+    public abstract void yourTurn();
+
+    public abstract void addCards();
+    public abstract List <Card> getHand();
+    public abstract void showPlayableCards(List <Card> l);
+
+}

5a3165af08b77e71e7324348a2e2f14124e92e0a
Alex <alexander.duca@gmail.com>
2017-04-19 23:53:39 +0200

    Bot minipush

--- core/src/de/emg_haar/schafkopfdeluxe/game/Bots/Bot.java
+++ core/src/de/emg_haar/schafkopfdeluxe/game/Bots/Bot.java
@@ -1,16 +1,34 @@
 package de.emg_haar.schafkopfdeluxe.game.Bots;
 import de.emg_haar.schafkopfdeluxe.game.Player;
-
-import de.emg_haar.schafkopfdeluxe.game.Player;
 import de.emg_haar.schafkopfdeluxe.game.card.Card;
 
 public class Bot extends Player {
 
-    protected Card[][] matrixBot;
-    public Bot()
-    {
-        super("Nathanael");
-        matrixBot = new Card[4][8];
+    /*
+
+    Die Attribute, die Bot erbt:
+
+    protected LinkedList<Card> hand;
+    protected String name;
+    protected Game game;
+    protected int points;
+    protected boolean player;
+    protected boolean wannaplay;
+    protected boolean online;
+    protected Stack<Card> stiche;
+    protected int punkte;
+    protected int stichanzahl;
+    protected boolean turn;
+
+    */
+
+    public Bot(){
+
+        super("Rudi");
+        wannaplay = false;  // Rudi spielt nie!
+
     }
+
+
 
 }
--- core/src/de/emg_haar/schafkopfdeluxe/game/Bots/Bot_1.java
+++ core/src/de/emg_haar/schafkopfdeluxe/game/Bots/Bot_1.java
@@ -2,7 +2,25 @@ package de.emg_haar.schafkopfdeluxe.game.Bots;
 
 public class Bot_1 extends Bot {
 
-    public Bot_1() {
+    /*
+
+    Die Attribute, die Bot_1 erbt:
+
+    protected LinkedList<Card> hand;
+    protected String name;
+    protected Game game;
+    protected int points;
+    protected boolean player;
+    protected boolean wannaplay;
+    protected boolean online;
+    protected Stack<Card> stiche;
+    protected int punkte;
+    protected int stichanzahl;
+    protected boolean turn;
+
+    */
+
+    public Bot_1(){
         this.name = "Faust";
     }
 

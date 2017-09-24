69e5df31871da488482ad531d57857209a78c638
alul_000 <alul@gmx.net>
2017-09-24 13:12:06 +0200

    1.2 Zweiter Anfang der KI Implementierung

--- core/src/de/emg_haar/schafkopfdeluxe/game/Bots/Bot.java
+++ core/src/de/emg_haar/schafkopfdeluxe/game/Bots/Bot.java
@@ -24,11 +24,11 @@ public class Bot implements Player
     private int spielender;
     private int playernumber;
 
-    Bot()
+    Bot(Game game1)
     {
         name = "Fuzzy";
         wannaplay = false;
-        game = null;
+        game = game1;
         points = 0;
         player = false;
         hand = new LinkedList<>();
@@ -191,19 +191,18 @@ public class Bot implements Player
     }
 
 
-    public Card legeKarte()
-    {
+    //public Card legeKarte()
+    //{
 
-    }
+    //}
     //---------------------------Anfang Fuzzy Methoden-------------------------------------------
     //Fuzzyfiziert die Punktzahl.
     //Die Rueckgabe ist ein Array mit der Zugehoerigkeit der Punktzahl zu fuenf verschiedenen linguistischen Variablen.
     //Genannte Variablen sind in Ihrer "Positivitaet" aufsteigend angeordnet.
-    public double[] fuzzyPunkte()
+
+    private double[] fuzzyPunkte(int punkte, int maximalePunktzahlImStich, boolean stichGehoertUns)
     {
         //Wenn mir der Stich nicht gehoert, dann gibt es keine Punkte zu holen:
-        boolean stichGehoertUns = false;
-        int maximalePunktzahlImStich;
         double[] toReturn = new double[5];
         if(!stichGehoertUns)
         {
@@ -218,30 +217,218 @@ public class Bot implements Player
         }
         if (maximalePunktzahlImStich < 20) {
             //In diesem Fall haben wir eine gestauchte Fuzzy-Akkordeonsfunktion:
-            return fuzzyAkkordeonsfunktion(punkte, maximalePunktzahlImStich);
+            return fuzzyAkkordeonsfunktion((double) punkte, (double) maximalePunktzahlImStich);
         }
-        else if (maximalePunktzahlImStich >= 20)
+        else
         {//In diesem Fall haben wir eine ungestauchteFuzzy-Akkordeonsfunktion über das Intervall [0, 20]:
-            if punkte< 20:
-            return fuzzyAkkordeonsfunktion(punkte, 20);
+            if (punkte< 20) {
+                return fuzzyAkkordeonsfunktion((double) punkte, 20.00);
+            }
+            else
+            {
+                toReturn[0] = 0;
+                toReturn[1] = 0;
+                toReturn[2] = 0;
+                toReturn[3] = 0;
+                toReturn[4] = 1;
+                return toReturn;
+            }
         }
         //Ist die Punktzahl groesser 20, so liegen eindeutig viele Punkte im Stich:
-        else if (punkte >= 20)
-        {
-            toReturn[0] = 0;
-            toReturn[1] = 0;
-            toReturn[2] = 0;
-            toReturn[3] = 0;
-            toReturn[4] = 1;
-            return toReturn;
+    }
+
+    /**
+     * Fuzzyfiziert die Staerke einer Karte.
+     * Die Rueckgabe ist ein Array mit der Zugehoerigkeit der Karte zu fuenf verschiedenen linguistischen Variablen.
+     * Genannte Variablen sind in Ihrer "Positivitaet" aufsteigend angeordnet.
+     * Es ist anzumerken, dass einer Karte eine Zahl zugeordnet werden muss, die bewertet werden soll.
+     */
+    private double[] fuzzyStaerke(double staerke, double anzahlUnterschiedlicheKarten) {
+        return fuzzyAkkordeonsfunktion(staerke, anzahlUnterschiedlicheKarten);
+    }
+
+    /**
+     *  Allgemeine Fuzzy-Funktion, die abhaengig von einem Maximalwert ist und wie ein Akkordeon aussieht.
+     * Es werden fuenf lingusitische Variablen auf das Intervall [0, maximalwert] gestaucht.
+     * Die Rueckgabe ist ein Array mit der Zugehoerigkeit der Zahl x zu den fuenf verschiedenen linguistischen Variablen.
+     */
+    private double[] fuzzyAkkordeonsfunktion(double x, double maximalwert) {
+        //Vorbereitung des Rueckgabe-Arrays
+        double[] Zugehoerigkeiten = new double[5];
+        //Wir unterteilen das Intervall [0, maximalwert] in vier kleine Abschnitte
+        double abschnittslaenge = maximalwert / 4;
+        //Wir schauen uns den Peak aller fuenf linguistischen Variablen an und defenieren and diesen eine Dreiecksfunktion
+        //Anschließend werfen wir unsere Punktzahl(x - Wert) gleich in genannte Funktion und speichern das Ergebnis.
+        for(int i = 0; i<5; i++) {
+            double peak = abschnittslaenge * (double) i;
+
+            Zugehoerigkeiten[i] = dreiecksfunktion(x, peak - abschnittslaenge, peak, peak + abschnittslaenge);
         }
+        //Rueckgabe
+        return Zugehoerigkeiten;
+    }
+
+    /**
+     * Allgemeine Dreiecksfunktion.
+     * Die Rueckgabe ist der y-Wert der Dreiecksfunktion
+     * ( fuer Duca: s. DSA Skript S. 16 )
+     */
+
+
+    private double dreiecksfunktion(double x, double anfangspunkt, double peak, double endpunkt)
+    {
+        if (x<anfangspunkt) {
+            return 0.0;
+        }
+        else if (anfangspunkt <=x && x <= peak)
+        {
+            return (x - anfangspunkt) / (peak - anfangspunkt);
+        }
+        else if (peak <x && x <= endpunkt)
+        {
+            return (endpunkt - x) / (endpunkt - peak);
+        }
+        else
+        {
+            return 0.0;
+        }
+    }
+
+    /**
+     * Allgemeine R-Funktion, erinnert an einen Hockey-Schlaeger.
+     * Die Rueckgabe ist der y-Wert der R-Funktion.
+     * ( fuer Duca: s. DSA Skript S. 18 )
+     */
+
+    private double  R_Funktion(double x, double anfangswert, double peak) {
+
+        if (x<anfangswert)
+        {
+            return 0.0;
+        }
+        else if (anfangswert <=x && x<peak)
+        {
+            return (x - anfangswert) / (peak - anfangswert);
+        }
+        else
+        {
+            return 1.0;
+        }
+    }
+
+    /**
+     * Allgemeine L-Funktion, erinnert auch an einen Hockey-Schlaeger.
+     * Die Rueckgabe ist der y-Wert der L-Funktion.
+     * ( fuer Duca: s. DSA Skript S. 20 )
+     */
+
+    private double L_Funktion(double x, double peak, double endwert) {
+
+        if (x<peak) {
+            return 1.0;
+        }
+        else if (peak <=x && x <= endwert) {
+            return (endwert - x) / (endwert - peak);
+        }
+        else
+        {
+            return 0.0;
+        }
+    }
+
+    /**
+     * s- und t-Norm:
+     * Gibt die kleinere der zwei Zahlen a und b zurueck.
+     */
+
+
+    private double min(double a, double b) {
+        if (a<b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+
+    /**
+     * Gibt die groessere der zwei Zahlen a und b zurueck.
+     */
+    private double max(double a, double b) {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+
+    /**
+     * Nach Mini-Max-Prinzip soll nun das Regelsystem aufgestellt werden.
+     * Die Rueckgabe ist ein Array mit den zwei Fuzzywerten "Ja" und "Nein"
+     */
+    private double[] regelsystem(double[] fuzzyPunkte, double[] fuzzyStaerke) {
+        //Das Regelsystem ist eine Einheitsmatrix die bewertet, ob eine Karte gelegt werden sollte.
+        //( fuer Duca: Die Digonale sagt "Ja" und alle anderen Werte sagen "Nein". )
+        double[] toReturn = new double[2];
+        //x- und y-"Koordinaten" der Matrix
+        for (int x = 0; x < 5; x++) {
+            for (int y = 0; y < 5; y++) {
+                //Diagonale:
+                if (x == y) {
+                    toReturn[0] = max(toReturn[0], min(fuzzyPunkte[x], fuzzyStaerke[y]));
+                }
+                //Alles andere:
+                else
+                {
+                    toReturn[1] = max(toReturn[1], min(fuzzyPunkte[x], fuzzyStaerke[y]));
+                }
+            }
+        }
+
+        return toReturn;
+    }
+
+    /**
+     * Defuzzyfizierung:
+     * Wir definieren zunächst die Flaeche, deren Schwerpunkt wir berechnen wollen:
+     */
+
+    private double defuzzyfunktion(double x, double Ja, double Nein) {
+        return max(min(Ja, R_Funktion(x, 0, 1)), min(Nein, L_Funktion(x, 0, 1)));
+    }
+
+    /**
+     * Gibt den x-Wert des Schwerpunkts zurueck. (Integralannaeherung)
+     */
+
+    private double berechneSchwerpunktDerDefuzzyfunktion(double Ja, double Nein) {
+        double o = 0.0;
+        double u = 0.0;
+        double w = 1000.0;
+        for (int d = 0; d<w; d++) {
+            double x = (double )d / w;
+            o = o + x * defuzzyfunktion(x, Ja, Nein);
+            u = u + defuzzyfunktion(x, Ja, Nein);
+        }
+        return o / u;
+    }
+
+    public double fuzzy(int punkte, double staerke, int maximalePunktzahlImStich, boolean stichGehoertUns, double anzahlUnterschiedlicheKarten) {
+        double[] L;
+        L = regelsystem(fuzzyPunkte(punkte, maximalePunktzahlImStich, stichGehoertUns), fuzzyStaerke(staerke, anzahlUnterschiedlicheKarten));
+        return berechneSchwerpunktDerDefuzzyfunktion(L[0], L[1]);
     }
     //---------------------------Ende Fuzzy Methoden---------------------------------------------
     //---------------------------Anfang Spielmechanik Methoden-----------------------------------
-    public boolean stichGehoertUns()
-    {
+    //public boolean stichGehoertUns()
+    //{
 
-    }
+    //}
 
 
     //---------------------------Ende Spielmechanik Methoden-------------------------------------
